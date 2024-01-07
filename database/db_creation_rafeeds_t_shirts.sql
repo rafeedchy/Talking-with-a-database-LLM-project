374dd2e2a712ada456ef573ce468f05b87d472e6
MD RAFEEDUL BAR CHOWDHURY <rafeedchy94@gmail.com>
2024-01-07 11:42:47 +0200

    llm and queries with db

--- database/db_creation_rafeeds_t_shirts.sql
+++ database/db_creation_rafeeds_t_shirts.sql
@@ -60,15 +60,3 @@ DELIMITER ;
 CALL PopulateTShirts();
 
 -- Insert at least 10 records into the discounts table
-INSERT INTO discounts (t_shirt_id, pct_discount)
-VALUES
-(1, 10.00),
-(2, 15.00),
-(3, 20.00),
-(4, 5.00),
-(5, 25.00),
-(6, 10.00),
-(7, 30.00),
-(8, 35.00),
-(9, 40.00),
-(10, 45.00);
